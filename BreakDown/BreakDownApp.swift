42ee4454e66c7ff7d99bc4a7beed8f595ebbf05c
akshaypkbhat <akshaypkbhat@gmail.com>
2025-01-23 14:17:23 -0800

    Initial Commit

---
+++ BreakDown/BreakDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakDownApp.swift
+//  BreakDown
+//
+//  Created by Akshay Bhatia on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct BreakDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BreakDownTests/BreakDownTests.swift
@@ -0,0 +1,17 @@
+//
+//  BreakDownTests.swift
+//  BreakDownTests
+//
+//  Created by Akshay Bhatia on 1/23/25.
+//
+
+import Testing
+@testable import BreakDown
+
+struct BreakDownTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
